a5de880cf7eac054cecfbc4648ffecaa1b76696c
4NF0 <148747658+4NF0@users.noreply.github.com>
2023-10-30 13:20:43 +0700

    Commit

---
+++ Latihan18Klip.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan18.klip;
+
+/**
+ *
+ * @author nm
+ */
+public class Latihan18Klip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         for (double i = 3.5; i <= 35; i += 3.5) {
+            if (i % 3 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
